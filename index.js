856c1cd77cc75937111ac8cc1b4354636cc907ba
wachira <wewaruts@gmail.com>
2024-03-26 01:26:45 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 *num2;
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 24
+let num4 = 5
+let mod = num3%num4;  //modulus operator(%)--aka remainder operator
+
+
+let max = Math.max(4,9,17,20);
+
+
